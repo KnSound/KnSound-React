43efd85ef2c746e28d7d9d64b3b1164e0391a7d9
BiLaNuK <bilanuk.bill@gmail.com>
2022-12-19 20:01:39 +0200

    change back url to heroku

--- src/redux/knSoundApi.js
+++ src/redux/knSoundApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const KnSoundApiUrl = 'http://localhost:3000/api'
+export const KnSoundApiUrl = 'https://knsound-rails.herokuapp.com/api/'
 
 export const knSoundApi = createApi({
     reducerPath: 'knSoundApi',
